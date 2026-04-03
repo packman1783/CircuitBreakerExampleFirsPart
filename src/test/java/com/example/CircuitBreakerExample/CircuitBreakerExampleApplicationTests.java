245b92dbb7596b7f2c4bc85caf345173a08e3210
Denis Saplitsyn <d.saplitsyn@mail.astondevs.ru>
2026-04-03 11:02:24 +0300

    создан CircuitBreakerExampleApplicationTests заглушка

---
+++ src/test/java/com/example/CircuitBreakerExample/CircuitBreakerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CircuitBreakerExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
